4f16ffc59bc3818b7ba3c14b1ecaa0f8fc3c8090
daisuke <dada.00131@google.com>
2019-04-23 20:35:12 +0900

    modified createarticle

--- db/migrate/20190423092406_create_articles.rb
+++ db/migrate/20190423092406_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.integer :body, default: 0
+      t.integer :status, default: 0
       t.references :user, foreign_key: true
 
       t.timestamps

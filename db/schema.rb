b166533c9d7c8875ce1c51a98be6c620093c11bb
daisuke <dada.00131@google.com>
2019-04-23 20:36:20 +0900

    add articles status

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,8 @@ ActiveRecord::Schema.define(version: 2019_04_23_092501) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
-    t.integer "body", default: 0
+    t.text "body"
+    t.integer "status", default: 0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

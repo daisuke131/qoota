74d27aa6e368b1debec5667fb964e1dc5bb5f202
daisuke <dada.00131@google.com>
2019-04-29 17:49:19 +0900

    articles factorybot修正

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
+    title { Faker::Lorem.sentence }
+    body { Faker::Markdown.emphasis }
+    status { Faker::Number.within(0..2) }
   end
 end

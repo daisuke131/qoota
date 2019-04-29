7066dea64ee5f777d37a01a82ab2deb3a201868c
daisuke <dada.00131@google.com>
2019-04-29 21:29:45 +0900

    users factorybot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password { "MyString" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
   end
 end

bda4cb9bce86a777c6f7d4107b686afbc80f7f74
daisuke <dada.00131@google.com>
2019-05-04 00:50:36 +0900

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, { format: "json" } do
     namespace :v1 do
       resources :articles
+    end
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+          registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end

ed5eb91b570c9a3dc4f1a2e94fd24b703f8794b0
daisuke <dada.00131@google.com>
2019-05-04 00:49:24 +0900

    DeviseTokenAuthファイル生成

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,28 @@ ActiveRecord::Schema.define(version: 2019_04_23_092501) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
+    t.string "nickname"
+    t.string "image"
     t.string "email"
-    t.string "password"
+    t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "articles", "users"

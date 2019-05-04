59a771f6a34a5eb9050a91a6b51a9e0c21c33b8a
daisuke <dada.00131@google.com>
2019-05-05 00:57:28 +0900

    regstrations_controller.rb修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,18 +1,10 @@
-module Api
-  module V1
-    module Auth
-      class RegistrationsController < DeviseTokenAuth::RegistrationsController
-
-        private
-        def sign_up_params
-          params.permit(:name, :email, :password, :password_confirmation)
-        end
-
-        def account_update_params
-          params.permit(:name, :email)
-        end
-
-      end
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+    def sign_up_params
+      params.permit(:name, :email, :password, :password_confirmation)
     end
-  end
+
+    def account_update_params
+      params.permit(:name, :email)
+    end
 end

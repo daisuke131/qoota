2076d0b273b4f27b228e337530cd4ecece9fd386
daisuke <dada.00131@google.com>
2019-05-08 12:46:00 +0900

    仮current_user削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
-
-  def current_user
-    @current_user ||= User.first
-  end
 end

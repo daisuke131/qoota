1050113f215ba322a8e12a48bd222dedb77e2ec3
daisuke <dada.00131@google.com>
2019-05-14 00:01:13 +0900

    create article_container.vue

---
+++ app/javascript/packs/components/article_container.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="article-container">
+    <div>タイトル：{{article.title}}</div>
+    <div>内容：{{article.body}}</div>
+    <router-link :to="{ name : 'post_edit', params : { id: article.id }}">修正</router-link>
+    <a class="nav-link" href="#" @click="delete_article">削除</a>
+  </div>
+</template>
+
+<script lang="ts">
+  import axios from "axios"
+  import { Vue, Component } from "vue-property-decorator"
+
+  @Component
+  export default class ArticleContainer extends Vue {
+    article: String[] = []
+    currentStorage = {
+      headers: {
+        "access-token": localStorage["access-token"],
+        "client": localStorage["client"],
+        "uid": localStorage["uid"]
+      }
+    }
+
+    async mounted(): Promise<void> {
+      await this.fetchHome();
+    }
+
+    async fetchHome(): Promise<void> {
+      await axios.get("/api/v1/articles/" + this.$route.params.id).then((response) => {
+        this.article = response.data
+      })
+    }
+
+    async delete_article(): Promise<void> {
+      axios.delete("/api/v1/articles/" + this.$route.params.id, this.currentStorage).then((response) => {
+        alert("削除")
+        this.$router.push({ name: "home" })
+      })
+    }
+  }
+</script>
+
+<style scoped>
+ul li {
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  list-style: none;
+}
+</style>

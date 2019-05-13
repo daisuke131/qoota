201ec913ed1fce92ae6bd006647182aaaf3f1740
daisuke <dada.00131@google.com>
2019-05-14 00:02:16 +0900

    fix route

--- app/javascript/packs/router/router.ts
+++ app/javascript/packs/router/router.ts
@@ -2,12 +2,21 @@ import Vue from 'vue/dist/vue.esm.js'
 import VueRouter from 'vue-router'
 import HomeContainer from '../components/home_container.vue'
 import SigninContainer from '../components/signin_container.vue'
+import ArticleContainer from '../components/article_container.vue'
+import PostNewContainer from '../components/post_new_container.vue'
+import PostEditContainer from '../components/post_edit_container.vue'
 
 Vue.use(VueRouter)
-export default new VueRouter({
+
+const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', name: 'home', component: HomeContainer },
     { path: '/sign_in', name: 'sign_in', component: SigninContainer },
+    { path: '/article/:id', name: 'article', component: ArticleContainer },
+    { path: '/post/new', name: 'post_new', component: PostNewContainer, meta: { requiresAuth: true } },
+    { path: '/post/edit/:id', name: 'post_edit', component: PostEditContainer, meta: { requiresAuth: true } },
   ],
 })
+
+export default router

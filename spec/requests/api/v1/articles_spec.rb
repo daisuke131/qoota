7fdaddce1d6b0256a7efd8ed7ea74c50ef67d410
daisuke <dada.00131@google.com>
2019-05-14 14:59:49 +0900

    認証トークンの値の渡し方を修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -55,38 +55,42 @@ RSpec.describe "Api::V1::Articles", type: :request do
   end
 
   describe "POST /api/v1/articles" do
-    subject { post(api_v1_articles_path, params: params, headers: @auth_token) }
+    subject { post(api_v1_articles_path, params: params, headers: auth_token(current_user)) }
     let(:params) { { article: attributes_for(:article, status: "published") } }
+    let(:current_user) { create(:user) }
 
     it "記事のレコードが作成される" do
-      login
       expect { subject }.to change { Article.count }.by(1)
       expect(response).to have_http_status(204)
     end
   end
 
   describe "PATCH /api/v1/articles/:id" do
-    subject { patch(api_v1_article_path(@article.id), params: params, headers: @auth_token) }
+    subject { patch(api_v1_article_path(article.id), params: params, headers: auth_token(current_user)) }
+    let(:current_user) { create(:user) }
+    let(:article) { create(:article, user_id: current_user.id) }
     let(:params) { { article: { title: Faker::Lorem.sentence, created_at: Time.current } } }
 
     it "指定した記事のレコードが更新される" do
-      login
-      @article = create(:article, user_id: @current_user.id)
-      expect { subject }.to change { Article.find(@article.id).title }.from(@article.title).to(params[:article][:title])
-      expect { subject }.not_to change { Article.find(@article.id).body }
-      expect { subject }.not_to change { Article.find(@article.id).created_at }
+      expect { subject }.to change { Article.find(article.id).title }.from(article.title).to(params[:article][:title])
+      expect { subject }.not_to change { Article.find(article.id).body }
+      expect { subject }.not_to change { Article.find(article.id).created_at }
       expect(response).to have_http_status(204)
     end
   end
 
   describe "DELETE /api/v1/articles/:id" do
-    subject { delete(api_v1_article_path(@article.id), headers: @auth_token) }
+    subject { delete(api_v1_article_path(article.id), headers: auth_token(current_user)) }
+    let(:current_user) { create(:user) }
+    let!(:article) { create(:article, user_id: current_user.id) }
 
     it "指定した記事のレコードが削除される" do
-      login
-      @article = create(:article, user_id: @current_user.id)
       expect { subject }.to change { Article.count }.by(-1)
       expect(response).to have_http_status(204)
     end
+  end
+
+  def auth_token(current_user)
+    current_user.create_new_auth_token
   end
 end

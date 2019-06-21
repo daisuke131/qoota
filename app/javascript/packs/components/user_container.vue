473d677c6ed2cf0a3bd1fd97a73a3f37de8f51e9
daisuke <dada.00131@google.com>
2019-06-21 21:55:45 +0900

    ユーザー画面追加

---
+++ app/javascript/packs/components/user_container.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="user-container">
+    <h2>{{ user.name }}</h2>
+    <router-link :to="{ name: 'user_edit', params: { id: this.$route.params.id }}" class="btn btn-primary">ユーザー情報編集</router-link>
+    <b-tabs class="mt-5" content-class="mt-3">
+      <b-tab title="記事一覧" active>
+        <ul>
+          <li class="article" v-for="article in articles" :key="article.id">
+            <router-link :to="{ name : 'article', params: { id: article.id }}">
+              <div>{{ article.title }}</div>
+            </router-link>
+            <div>{{ article.body }}</div>
+          </li>
+        </ul>
+        <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+      </b-tab>
+      <b-tab title="いいね一覧">
+        <p>未実装</p>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+  import axios from "axios"
+  import { Vue, Component } from "vue-property-decorator"
+  import InfiniteLoading from "vue-infinite-loading"
+  Vue.use(InfiniteLoading);
+
+  @Component
+  export default class UserContainer extends Vue {
+    user = []
+    articles = []
+    articles_list = []
+    page = 0
+
+    //記事一覧無限スクロール
+    infiniteHandler($state) {
+      axios.get(`/api/v1/users/${ this.$route.params.id }`, {
+        params: {
+          page: this.page,
+        },
+      }).then(({ data }) => {
+        this.user = data
+        this.articles_list = data.articles.slice(this.page,this.page+10)
+        if (this.articles_list.length) {
+          this.articles.push(...this.articles_list);
+          this.page += 10;
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+.article {
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+}
+</style>

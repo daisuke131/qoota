7ce0886176ab820afac32332c1bbb1891ee6738f
daisuke <dada.00131@google.com>
2019-06-21 21:52:23 +0900

    サインアップ画面追加

---
+++ app/javascript/packs/components/signup_container.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="signup-container">
+    <div class="devise_form">
+      <div class="field">
+        <h2>Sign up!</h2>
+      </div>
+      <div class="error-message" v-for="errorMessage in errorMessages" :key="errorMessage">
+        {{ errorMessage }}
+      </div>
+      <form>
+        <div class="field">
+          <input type="text" name="name" placeholder="name" v-model="name">
+        </div>
+        <div class="field">
+          <input type="text" name="email" placeholder="email" v-model="email">
+        </div>
+        <div class="field">
+          <input type="password" name="password" placeholder="password" v-model="password">
+        </div>
+        <div class="field">
+          <button type="submit" class="btn btn-primary" @click.prevent="signUp">Sign up</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import axios from "axios"
+  import { Vue, Component } from "vue-property-decorator"
+  const SIGNIN_ERROR_MESSAGE = "メールアドレスかパスワードが間違ってますけども！"
+
+  @Component
+  export default class SignupContainer extends Vue {
+    name = ""
+    email = ""
+    password = ""
+    errorMessages=""
+
+    async signUp(): Promise<void> {
+      const params = { name: this.name, email: this.email, password: this.password }
+      await axios.post("/api/v1/auth", params ).then((response) => {
+        this.$router.push({ name: "sign_in" })
+      }).catch((error) => {
+        this.errorMessages = error.response.data.errors.full_messages
+      })
+    }
+  }
+</script>
